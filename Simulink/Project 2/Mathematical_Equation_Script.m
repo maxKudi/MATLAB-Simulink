bf5507174776aa7f1fb1611f2b6249582627956b
Kudi <talk2cudi@yahoo.com>
2020-10-01 22:54:08 +0200

    Mathematical Equation Differentiation/Integration System

---
+++ Simulink/Project 2/Mathematical_Equation_Script.m
@@ -0,0 +1,7 @@
+prompt = 'What is the Constant value of F ?';
+F = input(prompt);
+prompt1 = 'What is the Constant value of A ?';
+A = input(prompt1);
+prompt2 = 'What is the Constant value of Ti ?';
+Ti = input(prompt2);
+sim('Mathematical_Equation')
